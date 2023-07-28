78d66cc752c0f1091dda5d7b5890c26f66a17c99
Mariana Ramirez <marianasmatos1@gmail.com>
2023-07-28 00:16:48 -0500

    modificando animacion del nav

--- src/App.vue
+++ src/App.vue
@@ -231,11 +231,11 @@ document.addEventListener('click', el => {
 @keyframes aparecer {
   0% {
     opacity: 0;
-    transform: translateY(0%);
+    transform: translateY(100%);
   }
   100% {
     opacity: 1;
-    transform: translateY(100%);
+    transform: translateY(0);
   }
 }
 @keyframes crecer {
